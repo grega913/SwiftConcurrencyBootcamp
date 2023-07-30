848a61e6c414e40f4bd5d7f1a1486804d19521d9
gs <grega913@gmail.com>
2023-07-30 11:26:27 +0200

    TaskGroupBootcamp added

---
+++ SwiftConcurrencyBootcamp/TaskGroupBootcamp.swift
@@ -0,0 +1,155 @@
+//
+//  TaskGroupBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by gs on 30/07/2023.
+//
+
+import SwiftUI
+
+
+class TaskGroupBootcampDataManager {
+    
+    func fetchImagesWithAsyncLet() async throws -> [UIImage] {
+        async let fetchImage1 = fetchImage(urlString: "https://picsum.photos/300")
+        async let fetchImage2 = fetchImage(urlString: "https://picsum.photos/300")
+        async let fetchImage3 = fetchImage(urlString: "https://picsum.photos/300")
+        async let fetchImage4 = fetchImage(urlString: "https://picsum.photos/300")
+        
+        
+        let (image1, image2, image3, image4) = await(try fetchImage1, try fetchImage2, try fetchImage3, try fetchImage4)
+        
+        return [image1, image2, image3, image4]
+    }
+    
+    
+    
+    func fetchImagesWithTaskGroup() async throws -> [UIImage] {
+        
+        let urlStrings = [
+            "https://picsum.photos/200",
+            "https://picsum.photos/300",
+            "https://picsum.photos/400",
+            "https://picsum.photos/200",
+            "https://picsum.photos/300",
+            "https://picsum.photos/300"
+        ]
+        
+        
+        
+        return try await withThrowingTaskGroup(of: UIImage?.self) { group in
+            var images: [UIImage] = []
+            images.reserveCapacity(urlStrings.count)
+            
+            for urlString in urlStrings {
+                group.addTask {
+                    
+                    //if we add a ? after try, this means that result of fetchImage is optional, meaning that if we have 50 tasks, and 1 not completes, others still do
+                    try? await self.fetchImage(urlString: urlString)
+                }
+            }
+            
+            /*
+            group.addTask {
+                try await self.fetchImage(urlString: "https://picsum.photos/300")
+            }
+            group.addTask {
+                try await self.fetchImage(urlString: "https://picsum.photos/300")
+            }
+            
+            group.addTask {
+                try await self.fetchImage(urlString: "https://picsum.photos/300")
+            }
+            
+            group.addTask {
+                try await self.fetchImage(urlString: "https://picsum.photos/300")
+            }
+            */
+            // this waits for each of the tasks to come back
+            for try await image in group {
+                if let image = image {
+                    images.append(image)
+                }
+                
+            }
+            
+            
+            return images
+        }
+        
+        
+    }
+    
+    
+    private func fetchImage(urlString: String) async throws -> UIImage {
+        
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url, delegate: nil )
+            if let image = UIImage(data:data) {
+                return image
+            } else {
+                throw URLError(.badURL)
+            }
+        } catch {
+            throw error
+        }
+    }
+    
+    
+    
+}
+
+class TaskGroupBootcampViewModel: ObservableObject {
+    
+    @Published var images: [UIImage] = []
+    let manager = TaskGroupBootcampDataManager()
+    
+    func getImages() async {
+//        if let images = try? await manager.fetchImagesWithAsyncLet() {
+//            self.images.append(contentsOf: images)
+//        }
+        
+        if let images = try? await manager.fetchImagesWithTaskGroup() {
+            self.images.append(contentsOf: images)
+        }
+    }
+}
+
+
+struct TaskGroupBootcamp: View {
+    
+    @StateObject private var viewModel = TaskGroupBootcampViewModel()
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    
+    
+ 
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(viewModel.images, id: \.self) { image in
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height:150)
+                    }
+                }
+            }
+            .navigationTitle("Task group")
+            .task {
+                await viewModel.getImages()
+            }
+        }
+    }
+}
+
+struct TaskGroupBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskGroupBootcamp()
+    }
+}
